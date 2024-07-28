81d38d23ae48def82daaa3297e34decac5ad38ba
divyeshp07 <divyesh5262@gmail.com>
2024-07-29 00:44:28 +0530

    employee model added

---
+++ lib/model/employee_model.dart
@@ -0,0 +1,31 @@
+class Employee {
+  int id;
+  String employeeName;
+  int employeeAge;
+  double employeeSalary;
+
+  Employee({
+    required this.id,
+    required this.employeeName,
+    required this.employeeAge,
+    required this.employeeSalary,
+  });
+
+  factory Employee.fromJson(Map<String, dynamic> json) {
+    return Employee(
+      id: json['id'],
+      employeeName: json['employee_name'],
+      employeeAge: json['employee_age'],
+      employeeSalary: json['employee_salary'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'employee_name': employeeName,
+      'employee_age': employeeAge,
+      'employee_salary': employeeSalary,
+    };
+  }
+}
